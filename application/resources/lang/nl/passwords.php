4d25728ee2fdcb8a48b62324a96a067c02dea3fd
Xander Smeets <xsmeets@discant.net>
2015-08-20 14:04:50 +0200

    Translated passwords.php

---
+++ application/resources/lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Wachtwoorden moeten minstens zes tekens lang zijn en moeten overeenkomen met de bevestiging.",
+	"user" => "We kunnen geen gebruiker met dat e-mailadres vinden.",
+	"token" => "Dit wachtwoordhersteltoken is ongeldig.",
+	"sent" => "We hebben je via e-mail een wachtwoordherstellink toegestuurd!",
+	"reset" => "Je wachtwoord is opnieuw ingesteld!",
+
+];
